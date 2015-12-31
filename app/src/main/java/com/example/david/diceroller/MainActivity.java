02d4e83b80af73e94c7eabb523c6a229e0794ea3
David <deldel@suddenlink.net>
2015-12-30 20:51:42 -0600

    Corrected dice rolling methods called when clicking the Roll button. Added functionality for buttons to be implemented when dialogue boxes become available from Cole's branch. In addition, used the loop method of getDice to clean up the code in the getBonus method. Both now include null checking.

--- app/src/main/java/com/example/david/diceroller/MainActivity.java
+++ app/src/main/java/com/example/david/diceroller/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
 
     MyDBHandler handle;
 
-    //
+    // k
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
